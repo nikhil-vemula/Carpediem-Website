b995dda57789d8b2806acb5d9e9cf37d022b9b9c
vsnick <vsrinivasanikhil@gmail.com>
2017-02-19 04:57:36 +0530

    choose your side added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 function clan_selected(clan){
     var titans_content = document.getElementById("titans-content");
     var vulcans_content = document.getElementById("vulcans-content");
+    var cys = document.getElementById("cys");
+    cys.style.display="none";
     if(clan=='titans'){
         titans_content.className='clan-selected';
     }
